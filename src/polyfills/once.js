4a32868380f271f3e7c0d973f0b2080ce831383f
anupampersad <58291632+anupampersad@users.noreply.github.com>
2024-11-26 13:01:27 +0530

    Update once.js

--- src/polyfills/once.js
+++ src/polyfills/once.js
@@ -1,9 +1,21 @@
+// function myOnce(fn, context) {
+//   let ran = false;
+//   return function (...args) {
+//     if (!ran) {
+//       ran = true;
+//       return fn.call(context || this, ...args);
+//     } else {
+//       return;
+//     }
+//   };
+// }
+
 function myOnce(fn, context) {
   let ran = false;
-  return function (...args) {
+  return function () {
     if (!ran) {
       ran = true;
-      return fn.call(context || this, ...args);
+      return fn.apply(context || this, arguments); // arguments is a list of args present inside the func
     } else {
       return;
     }
